1c23da1a0c0895e63a395962df627a2a2bc1cb5e
Nikita <lukashevich-nikitka@bk.ru>
2022-07-08 00:26:24 +0300

    feat: add edition users data

--- client/src/pages/friends_list/components/user_item.jsx
+++ client/src/pages/friends_list/components/user_item.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function UserItem(props) {
+    const [newName, setNewName] = useState('')
+    const [newSurname, setNewSurname] = useState('')
+    const [editClick, setEditClick] = useState(true)
     const { name, surname, id } = props;
     const deleteUser = function() {
         axios.delete(`http://localhost:4200/api/getter/delete/${id}`)
     }
-    return (
-        <>
-        <div className="user-item-wrapper">
-        <div className="user-item">{name} {surname}</div>
-        <div className="user-buttons">
-            {/* СДЕЛАЙ ПЕРЕЗАПИСЬ БАЗЫ ДАННЫХ ПОСЛЕ УДАЛЕНИЯ ПОЛЬЗОВАТЕЛЯ */}
-        <button className="user-item-delete-button" onClick={deleteUser}>Delete</button>
-        <button className="user-item-update-button">Update</button>
-        </div>
-        </div>
-        </>
-    )
+    const editUser = function() {
+        setEditClick(false)
+    }
+    const confirmEditUser = function() {
+        axios.put(`http://localhost:4200/api/getter/edit/${id}`, {newLogin: newName, newPassword: newSurname})
+        setEditClick(true)
+    }
+    const rewriteName = (event) => setNewName(event.target.value)
+    const rewriteSurname = (event) => setNewSurname(event.target.value)
+    if (editClick) {
+        return (
+            <>
+            <div className="user-item-wrapper">
+            <div className="user-item">{name} {surname}</div>
+            <div className="user-buttons">
+            <button className="user-item-delete-button" onClick={deleteUser}>Delete</button>
+            <button className="user-item-update-button" onClick={editUser}>Edit</button>
+            </div>
+            </div>
+            </>
+        ) 
+    } else {
+        return (
+            <>
+            <div className="user-item-wrapper">
+            <div className="edit-input-wrapper">
+            <input type="text" placeholder="Name" className="edit-input" onChange={rewriteName}></input>
+            <input type="text" placeholder="Surname" className="edit-input" onChange={rewriteSurname}></input>
+            </div>
+            <div className="user-buttons">
+            <button className="user-item-update-button" onClick={confirmEditUser}>Confirm</button>
+            </div>
+            </div>
+            </>
+        )
+    }
 }
 
 export default UserItem;
--- routes/data_processing.js
+++ routes/data_processing.js
@@ -4,11 +4,6 @@ const database = require('../database.json');
 
 const router = Router() 
 
-// router.get('/data', async (req, res) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.send({id: 7, name: "Nikita"});
-// })
-
 router.post('/registration', async (req, res) => {
     const newDataBase = JSON.stringify([...database, { login: req.body.login, password: req.body.password }]);
     fs.writeFile("database.json", newDataBase, function(err){
@@ -24,6 +19,13 @@ router.get('/users', async (req, res) => {
 
 router.delete('/delete/:id', async(req, res) => {
     database.splice(req.params.id , 1);
+})
+
+router.put('/edit/:id', async(req, res) => {
+    database[req.params.id] = { login: req.body.newLogin, password: req.body.newPassword}
+    fs.writeFile("database.json", JSON.stringify(database), function(err){
+        if(err)throw err;
+    })
 })
 
 module.exports = router
